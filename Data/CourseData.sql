9f747072cb4ce8ee9646adfb9060b4d7ea3422f2
nextgeneratio <currstud23123@gmail.com>
2026-05-03 08:59:17 +0530

    Add SQL table creation scripts for Admin, Attendance, Course, Dean, Department, Final_Marks, LecturesDesignation, LecturesPhone, Lecturers, Medical, Score, Student, StudentCourse, TechnicalOfficer, and UserAccount

--- Data/CourseData.sql
+++ Data/CourseData.sql
@@ -1,9 +1,9 @@
 INSERT INTO Course 
-(Course_ID, Name, No_of_hours, Contact_NO, Level, Type, Semester, Credit, Dept_ID) 
+(Course_ID, Name, No_of_hours, Level, Type, Semester, Credit, Dept_ID, Lec_ID) 
 VALUES
-('DBS201', 'Database Systems', 60, '0712345678', 'L3', 'Core', '01', 3, 1),
-('WED202', 'Web Development', 75, '0723456789', 'L3', 'Core', '01', 4, 1),
-('SWE203', 'Software Engineering', 80, '0734567890', 'L4', 'Core', '02', 4, 2),
-('CNR204', 'Computer Networks', 65, '0745678901', 'L2', 'Core', '01', 3, 1),
-('BIO205', 'Bioinformatics', 70, '0756789012', 'L3', 'Optional', '02', 3, 3)
+('DBS201', 'Database Systems', 60, 'L3', 'Theory', '01', 3, 1, "UA002"),
+('WED202', 'Web Development', 75, 'L3', 'Theory', '01', 4, 1, "UA009"),
+('SWE203', 'Software Engineering', 80, 'L4', 'Theory', '02', 4, 2, "UA008"),
+('CNR204', 'Computer Networks', 65, 'L2', 'Theory', '01', 3, 1, "UA005"),
+('BIO205', 'Bioinformatics', 70, 'L3', 'Practical', '02', 3, 3, "UA009")
 ;
--- Table-Creation/Course.sql
+++ Table-Creation/Course.sql
@@ -4,13 +4,16 @@ CREATE TABLE Course (
     Course_ID VARCHAR(20) PRIMARY KEY,
     Name varchar(50),
     No_of_hours int,
-    Contact_NO varchar(10),
     Level varchar(3),
     Type varchar(20),
     Semester varchar(2),
     Credit int,
     Dept_ID int,
     FOREIGN KEY (Dept_ID) REFERENCES Department(Dept_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    Lec_ID VARCHAR(20),
+    FOREIGN KEY (Lec_ID) REFERENCES Lecturers(user_ID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
